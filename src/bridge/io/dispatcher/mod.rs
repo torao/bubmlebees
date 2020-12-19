ac051275754ba304fc8d64827002a0f7b2864c30
TAKAMI Torao <koiroha@gmail.com>
2020-12-20 06:26:11 +0900

    M

--- src/bridge/io/dispatcher/mod.rs
+++ src/bridge/io/dispatcher/mod.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 use std::future::Future;
-use std::ops::{DerefMut};
+use std::ops::DerefMut;
 use std::pin::Pin;
-use std::sync::{Arc, Mutex, RwLock};
+use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::task::{Context, Waker};
 use std::thread::{JoinHandle, spawn};
 
@@ -14,9 +15,166 @@ use mio::net::{TcpListener, TcpStream};
 
 use crate::error::Error;
 use crate::Result;
-use std::sync::mpsc::{Sender, channel, Receiver};
 
-/// Poll に登録するためのソケットを格納する列挙型。
+type Executable<R> = dyn (FnMut(&mut Poll, &mut SocketMap) -> R) + Send + 'static;
+
+struct TaskState<R> {
+  result: Option<R>,
+  waker: Option<Waker>,
+}
+
+struct Task<R> {
+  executable: Box<Executable<R>>,
+  state: Arc<Mutex<TaskState<R>>>,
+}
+
+impl<R> Task<R> {
+  fn new<E>(executable: E) -> Self where E: (FnMut(&mut Poll, &mut SocketMap) -> R) + Send + 'static {
+    Self {
+      executable: Box::new(executable),
+      state: Arc::new(Mutex::new(TaskState {
+        result: None,
+        waker: None,
+      })),
+    }
+  }
+  fn execute(&mut self, poll: &mut Poll, sockets: &mut SocketMap) {
+    let result = (self.executable)(poll, sockets);
+    let mut state = self.state.lock().unwrap();
+    state.result = Some(result);
+    if let Some(waker) = state.waker.take() {
+      waker.wake();
+    }
+  }
+}
+
+struct TaskFuture<R> {
+  state: Arc<Mutex<TaskState<R>>>
+}
+
+impl<R> Future for TaskFuture<R> {
+  type Output = R;
+
+  fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> std::task::Poll<Self::Output> {
+    use std::task::Poll;
+    let mut state = self.state.lock().unwrap();
+    if let Some(result) = state.result.take() {
+      Poll::Ready(result)
+    } else {
+      state.waker = Some(cx.waker().clone());
+      Poll::Pending
+    }
+  }
+}
+
+pub type SocketId = usize;
+
+pub struct Dispatcher {
+  event_thread: JoinHandle<Result<()>>,
+  sender: Sender<Task<Result<SocketId>>>,
+  waker: mio::Waker,
+  is_closed: Arc<AtomicBool>,
+}
+
+impl Drop for Dispatcher {
+  fn drop(&mut self) {
+    self.stop().unwrap();
+  }
+}
+
+impl Dispatcher {
+  pub fn new(event_buffer_size: usize) -> Result<Dispatcher> {
+    let (sender, receiver) = channel();
+    let mut poll = Poll::new()?;
+    let waker = mio::Waker::new(poll.registry(), Token(0))?;
+
+    let is_closed = Arc::new(AtomicBool::new(false));
+    let is_closed_cloned = is_closed.clone();
+    let event_thread = spawn(move || {
+      Dispatcher::event_loop(receiver, poll, is_closed_cloned, event_buffer_size)
+    });
+    Ok(Dispatcher { event_thread, sender, waker, is_closed })
+  }
+
+  pub fn stop(&mut self) -> Result<()> {
+    if self.is_closed.compare_and_swap(false, true, Ordering::SeqCst) {
+      log::info!("stopping TCP bridge...");
+      self.waker.wake()?;
+    }
+    Ok(())
+  }
+
+  fn run_in_event_loop(&mut self, exec: fn(&mut Poll, &mut SocketMap) -> Result<SocketId>) -> TaskFuture<Result<SocketId>> {
+    let task = Task::new(exec);
+    let future = TaskFuture { state: task.state.clone() };
+    self.sender.send(task).unwrap();
+    self.waker.wake().unwrap();
+    future
+  }
+
+  fn event_loop<R>(receiver: Receiver<Task<R>>, mut poll: Poll, is_closed: Arc<AtomicBool>, event_buffer_size: usize) -> Result<()> {
+    let mut events = Events::with_capacity(event_buffer_size);
+    let mut sockets = SocketMap::new();
+
+    while is_closed.load(Ordering::SeqCst) {
+      poll.poll(&mut events, None)?;
+
+      // イベントの発生したソケットを取得
+      let event_sockets = events.iter()
+        .map(|e| sockets.get(e.token().0).map(|s| (e, s)))
+        .flatten().collect::<Vec<(&Event, Arc<Mutex<Socket>>)>>();
+
+      // イベントの発生したソケットの処理を実行
+      for (event, socket) in event_sockets.iter() {
+        match socket.lock()?.deref_mut() {
+          Socket::Stream(stream) => {
+            log::info!("CLIENT");
+          }
+          Socket::Listener(listener) => {
+            log::info!("SERVER");
+          }
+          Socket::Waker => {
+            log::info!("WAKER");
+          }
+        }
+      }
+
+      // タスクキューに入っている処理をすべて実行
+      for mut task in receiver.iter() {
+        task.execute(&mut poll, &mut sockets);
+      }
+    }
+    Ok(())
+  }
+}
+
+pub trait DispatcherRegister<T> {
+  fn register<F>(&mut self, stream: T) -> F where F: Future<Output=Result<SocketId>>;
+}
+
+impl DispatcherRegister<TcpListener> for Dispatcher {
+  fn register<F>(&mut self, mut listener: TcpListener) -> F where F: Future<Output=Result<SocketId>> {
+    self.run_in_event_loop(move |poll, sockets| {
+      let id = sockets.add(Socket::Listener(listener))?;
+      poll.registry()
+        .register(&mut listener, Token(id), Interest::READABLE)?;
+      Ok(id)
+    })
+  }
+}
+
+impl DispatcherRegister<TcpStream> for Dispatcher {
+  fn register<F>(&mut self, mut stream: TcpStream) -> F where F: Future<Output=Result<SocketId>> {
+    self.run_in_event_loop(move |poll, sockets| {
+      let id = sockets.add(Socket::Stream(stream))?;
+      poll.registry()
+        .register(&mut stream, Token(id), Interest::READABLE | Interest::WRITABLE)?;
+      Ok(id)
+    })
+  }
+}
+
+/// Poll に登録するソケットを格納する列挙型。
 enum Socket {
   Waker,
   Stream(TcpStream),
@@ -48,7 +206,7 @@ impl SocketMap {
   }
 
   /// 指定されたオブジェクトをこのマップに格納し、新しく発行された ID を返します。
-  pub fn add(&mut self, socket: Socket) -> Result<usize> {
+  pub fn add(&mut self, socket: Socket) -> Result<SocketId> {
     // NOTE Token(0) は Waker 用、Token(usize::MAX) は Poll が内部的に使用している
     let max = std::usize::MAX - 2;
     if self.sockets.len() == max {
@@ -68,181 +226,5 @@ impl SocketMap {
   /// 指定された ID のオブジェクトをこのマップから削除して返します。
   pub fn remove(&mut self, id: usize) -> Option<Arc<Mutex<Socket>>> {
     self.sockets.remove(&id)
-  }
-}
-
-pub struct Dispatcher {
-  event_thread: JoinHandle<Result<()>>,
-  sender: Sender<TASK>,
-  state: Arc<DispatcherState>,
-}
-
-type TASK = Box<dyn FnMut(&mut Poll, &mut SocketMap) + Send>;
-
-struct DispatcherState {
-  is_closed: AtomicBool,
-  receiver: Receiver<TASK>,
-  waker: mio::Waker,
-}
-
-impl Dispatcher {
-  pub fn new(event_buffer_size: usize) -> Result<Dispatcher> {
-    let (sender, receiver) = channel()?;
-    let poll = Mutex::new(Poll::new()?);
-    let waker = mio::Waker::new(poll.lock()?.registry(), Token(0))?;
-    let state = Arc::new(DispatcherState {
-      is_closed: AtomicBool::new(false),
-      receiver,
-      waker,
-    });
-
-    let cloned_state = state.clone();
-    let event_thread = spawn(move || {
-      cloned_state.event_loop(event_buffer_size)
-    });
-    Ok(Dispatcher { event_thread, sender, state })
-  }
-
-  pub fn stop(&mut self) -> Result<()> {
-    if self.state.is_closed.compare_and_swap(false, true, Ordering::SeqCst) {
-      log::info!("stopping TCP bridge...");
-      self.state.waker.wake()?;
-      // self.event_thread.join()?
-    }
-    Ok(())
-  }
-}
-
-impl DispatcherRegister<TcpListener> for Dispatcher {
-  fn register(&mut self, mut listener: TcpListener) -> Pin<Box<dyn Future<Output=usize>>> {
-    self.state.run_in_event_loop(|poll, sockets| {
-      let id = sockets.add(Socket::Listener(listener))?;
-      poll.registry().register(&mut listener, Token(id), Interest::READABLE).unwrap();
-      id
-    })
-  }
-}
-
-// impl DispatcherRegister<TcpStream> for Dispatcher {
-//   fn register(&mut self, mut stream: TcpStream) -> Pin<Box<dyn Future<Output=usize>>> {
-//     self.state.run_in_event_loop(|poll, sockets| {
-//       let id = sockets.add(socket)?;
-//       poll.registry().register(&mut stream, Token(id), Interest::READABLE).unwrap();
-//       id
-//     })
-//   }
-// }
-
-pub trait DispatcherRegister<T> {
-  fn register(&mut self, stream: T) -> Pin<Box<dyn Future<Output=usize>>>;
-}
-
-impl DispatcherState {
-  fn run_in_event_loop<R:'static>(&mut self, exec: fn(&mut Poll, &mut SocketMap) -> R) -> impl Future {
-    let mut queue = self.queue.write().unwrap();
-    let mut future = EventLoopResult::new();
-    queue.push(Box::new(move |poll, sockets| {
-      let result = exec(poll, sockets);
-      future.complete(result);
-    }));
-    self.waker.wake();
-    future
-  }
-
-  fn event_loop(&self, event_buffer_size: usize) -> Result<()> {
-    let mut poll = Poll::new()?;
-    let mut events = Events::with_capacity(event_buffer_size);
-    let mut sockets = SocketMap::new();
-
-    while self.is_closed.load(Ordering::SeqCst) {
-      poll.poll(&mut events, None)?;
-
-      // イベントの発生したソケットを取得
-      let event_sockets = events.iter()
-        .map(|e| sockets.get(e.token().0).map(|s| (e, s)))
-        .flatten().collect::<Vec<(&Event, Arc<Mutex<Socket>>)>>();
-
-      // イベントの発生したソケットの処理を実行
-      for (event, socket) in event_sockets.iter() {
-        match socket.lock()?.deref_mut() {
-          Socket::Stream(stream) => {
-            log::info!("CLIENT");
-          }
-          Socket::Listener(listener) => {
-            log::info!("SERVER");
-          }
-          Socket::Waker => {
-            log::info!("WAKER");
-          }
-        }
-      }
-
-      // キューに保存されている処理を実行
-      let mut queue = self.queue.write()?;
-      while let Some(exec) = queue.pop() {
-        exec(&mut poll, &mut sockets);
-      }
-    }
-    Ok(())
-  }
-
-  /// キューに保存されている登録待ちのソケットを Poll に登録します。
-  fn register_all(&self, poll: &mut Poll) -> Result<()> {
-    let mut queue = self.queue.write()?;
-    let sockets = self.sockets.read()?;
-    while let Some((token, socket)) = queue.pop() {
-      match socket.lock()?.deref_mut() {
-        Socket::Stream(mut stream) => {
-          poll.registry().register(&mut stream, token, Interest::READABLE | Interest::WRITABLE)?;
-        }
-        Socket::Listener(mut listener) => {
-          poll.registry().register(&mut listener, token, Interest::READABLE)?;
-        }
-        Socket::Waker => assert!(false, "Waker was inserted in queue")
-      }
-    }
-    Ok(())
-  }
-
-  /// 登録されているすべてのソケットを解除します。
-  fn deregister_all(&self) -> Result<()> {
-    // self.poll.registry().deregister()
-    Ok(())
-  }
-}
-
-struct EventLoopResult<R> {
-  result: RwLock<Vec<R>>,
-  waker: Option<Waker>,
-}
-
-impl<R> EventLoopResult<R> {
-  fn new() -> EventLoopResult<R> {
-    EventLoopResult {
-      result: RwLock::new(Vec::with_capacity(1)),
-      waker: None,
-    }
-  }
-  fn complete(&mut self, result: R) {
-    let mut buffer = self.result.write().unwrap();
-    if buffer.is_empty() {
-      buffer.push(result);
-    }
-  }
-}
-
-impl<R> Future for EventLoopResult<R> {
-  type Output = R;
-
-  fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> std::task::Poll<R> {
-    let buffer = self.result.read().unwrap();
-    if buffer.is_empty() {
-      if self.waker.is_none() {
-        self.waker = Some(cx.waker().clone());
-      }
-      std::task::Poll::Pending
-    } else {
-      std::task::Poll::Ready(buffer.get(0).unwrap())
-    }
   }
 }
